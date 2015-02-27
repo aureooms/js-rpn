6c6c934715f62a40f6e246f96911819a33b284c9
aureooms <aurelien.ooms@gmail.com>
2015-02-27 01:55:45 +0100

    v0.1.0

--- js/dist/rpn.js
+++ js/dist/rpn.js
@@ -3,6 +3,38 @@
 	'use strict';
 
 
-/* js/src/dummy.js */
+/* js/src/pull.js */
+
+var pull = function ( compile , isop , token , stack ) {
+
+	var a , b ;
+
+	if ( isop( token ) ) {
+
+		b  = stack.pop( ) ;
+		a  = stack.pop( ) ;
+		stack.push( compile( token , a , b ) ) ;
+
+	}
+
+	else {
+
+		stack.push( token ) ;
+
+	}
+
+} ;
+
+exports.pull = pull ;
+
+/* js/src/validate.js */
+
+var validate = function ( isop , token , stack ) {
+
+	return ! isop( token ) || stack.length >= 2 ;
+
+} ;
+
+exports.validate = validate ;
 
 })(typeof exports === 'undefined' ? this['rpn'] = {} : exports);
